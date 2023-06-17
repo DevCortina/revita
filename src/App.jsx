9ab56d516a19d3a6ca8bd98a978fc57b428cb525
DevCortina <ccortina@gmail.com>
2023-06-17 10:34:14 -0500

    Commit Inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-full'>
+      
+        <h1 className='text-3xl'> 
+          Hello App!
+        </h1>
+      
+    </div>
+  )
+}
+
+export default App
